fd4f7c7b50aae4064a33f3c12b69a303b53b0089
Willian Z <Willian@Willian-Zhang.com>
2017-04-21 20:04:59 -0400

    Update to python 3

--- osu_getInfo.py
+++ osu_getInfo.py
@@ -13,11 +13,11 @@ def search(input):
 		if (find):
 			for line in line_iter:
 				if len(line.strip()) != 0:
-					print line
+					print( line)
 				else: 
 					break			
 		else:
-			print "No such infomation in file"
+			print("No such infomation in file")
 
 def searchSublevel(x, y):
 	with open(sys.argv[1], 'r') as my_file:
@@ -34,21 +34,21 @@ def searchSublevel(x, y):
 				if len(line.strip()) != 0:
 					if y in line:
 						l, r = line.split(':')
-						print r
+						print( r)
 				else: 
 					"No such infomation in " + x
 					break			
 		else:
-			print "No such infomation in file"
+			print( "No such infomation in file")
 
 
 if __name__ == "__main__":
 	if len(sys.argv) != 2:
-		print "usage: inputfile outputfile"
+		print( "usage: inputfile outputfile")
 		exit(-1)
-	print "please enter the search information using the following format:"
-	print "e.g. General"
-	print "e.g. General-AudioFilename"
+	print( "please enter the search information using the following format:")
+	print( "e.g. General")
+	print( "e.g. General-AudioFilename")
 	x =  raw_input().split('-')
 	if len(x) == 1:
 		search(x[0])
