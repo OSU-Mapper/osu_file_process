75d159e216f3cea04dac94dc438f9e12bfb90351
Willian Z <Willian@Willian-Zhang.com>
2017-04-22 02:26:13 -0400

    Update for csv

--- osu-to-train.sh
+++ osu-to-train.sh
@@ -1,8 +1,12 @@
 #
 
-for path in Data/Beatmaps/*/*.osu ;do
-    echo "path"
-    # Put your code here
-    # e.g. python blah-blah.py "$path"
-    
+for path in $1/* ;do
+    # echo "path:$path"
+    csvpath="$path/feature-v1.csv"
+    for osupath in "$path"/*.osu  ; do
+        #echo "$osupath"
+        # Put your code here
+        # e.g. 
+        # python blah-blah.py "$osupath" "$csvpath"
+    done
 done
